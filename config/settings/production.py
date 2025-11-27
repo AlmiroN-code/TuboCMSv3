4aeb050c75a8a167888162e08aba2444248da67c
AlmiroN <33394132+AlmiroN-code@users.noreply.github.com>
2025-11-27 21:39:32 +0500

    fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -34,9 +34,6 @@ CACHES = {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": config("REDIS_URL", default="redis://localhost:6379/1"),
         "KEY_PREFIX": "rextube",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
     }
 }
 
